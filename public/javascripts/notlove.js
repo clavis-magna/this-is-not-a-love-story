65329abcbfc706299ead214969381eadd7f5f9de
Andrew Burrell <andrew@miscellanea.com>
2014-11-06 16:18:55 +1100

    working to create a first chapter (very rough and only using twitter as source material at the moment)

--- public/javascripts/notlove.js
+++ public/javascripts/notlove.js
@@ -1,30 +1,70 @@
+var $text = $( "#main_text" );
+var partName = [];
+var chunks = [];
+var chunkIterate = 1;
+
 $(doIt());
 
 function doIt(){
+
 	//get some text using the chunk api
 	$.get( "chunk", { chunktype : "phrase", chapter: "0" } )
+		//wait untit after we get the data back	
 		.done(function( data ) {
-			console.log(data.length);
-			
-			var $text = $( "#main_text" ),
-			partName = [];
-			
-			partName[0] = "<b><h2>CHAPTER 1 : <span class=\"chapter_name\">" + data[0] + "</span></h2></b>";
-			
-			//we start at 1 as the chapter title is at 0 in the array
-			for(var i = 1; i < data.length; i++){
-				//main part headings for chapter
-				partName[ i ] = "<li><b>CHAPTER 1, PART " + (i) + "</b>:  <br/><span class=\"chapter_name\">" + data[i] + "</span></li><br/><br/>";
-				//now add in some data for each chapter
-				//partName[ i ] += 
-				console.log(data[i]);
-			}
-			
-			// Insert the part names
-			$( "<ul></ul>" )
-			  .append( partName.join( "" ) )
-			  .appendTo( $text );
+			chunks = data;
+			//format the headings for partNames
+			formatHeadings();
   		});
+}
+
+function formatHeadings(){
+	
+	partName[0] = "<b><h2>CHAPTER 1 : <span class=\"chapter_name\">" + chunks[0] + "</span></h2></b>";
+			
+	//we start at 1 as the chapter title is at 0 in the array
+	for(var i = 1; i < chunks.length; i++){
+		//main part headings for chapter
+		partName[ i ] = "<li><b>CHAPTER 1, PART " + (i) + "</b>:  <br/><br/>" + chunks[i] + "</li>";
+	}
+	
+	//
+	//now add in some data for each chapter
+	//test
+	//
+	
+	//partName[1] += "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><br/><br/>";
+	
+	getTwitterContent();
+}
+
+function appendToPage(){
+	// Insert the part names
+	$( "<ul></ul>" )
+	  .append( partName.join( "" ) )
+	  .appendTo( $text );
+}
+
+function getTwitterContent(){
+	if(chunkIterate < chunks.length-1){
+		var theSearchTerm = chunks[chunkIterate].substr(0, 30);
+		console.log("**"+chunkIterate+"**"+chunks.length);
+		
+		$.get( "search", { term : theSearchTerm, number: "3" } )
+			.done(function(data){
+				console.log(data);
+				partName[chunkIterate] += "<p>" + data + "</p>";
+				chunkIterate ++;
+				setTimeout(getTwitterContent,5000);
+			})
+			.fail(function() {
+			    alert( "error" );
+			});
+	}
+	else{
+		console.log("should be writing to page now");
+		appendToPage();
+	}
+	
 }
 
 
--- routes/basicSearch.js
+++ routes/basicSearch.js
@@ -11,18 +11,70 @@ var T = new Twit(config);
 router.get('/', function(req, res) {
   		 
 	var term = req.query.term;
+	var numTweets = req.query.number;
+	
+	//we dont want retweets
+	term = term + " -RT";
+	
+	if(numTweets == undefined)
+	{
+		numTweets = 1;
+	}
 	
 	T.get('search/tweets', { q: term, count: 100 }, function(err, data) {
 		if(err) return handleError(err);
 		
 		//get the statuses from the json
 		var tweets = data.statuses;
+		var tweetsToReturn = "";
 		
-		//choose one at random
-        var i = tweets.length;
-        var j = randomInt(0, i);
-		res.send(tweets[j].text);
-      
+		if(tweets[0] != undefined){
+			
+			// store for indexes we have already chosen to check on repeats
+			// look into better random use (like a grab bag)
+			var usedTweets = [];
+			
+			for(var k = 0; k < numTweets; k++){
+				//choose one at random
+		        var i = tweets.length;
+		        var j = randomInt(0, i);
+	        
+				if(usedTweets.indexOf(j) == -1){
+					console.log(">>>> "+j);
+					//add index to usedTweets
+					usedTweets[k] = j;
+						
+			        var theTweet = tweets[j].text;
+			        
+			        //
+			        // do some tidying with regex - could push these together but easier to see whats going on when they are sepearte
+			        //
+			        
+			        //remove @s
+			        theTweet = theTweet.replace(/(@\w+)/ig, "");
+			        //remove RT : (just incase any retweets got through)
+			        theTweet = theTweet.replace(/(RT :+)/ig, "");
+			        //remove #tags
+					theTweet = theTweet.replace(/(#\w+)/g, "");
+			        //remove URLS
+			        theTweet = theTweet.replace(/((ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?)/ig, "");
+			        
+			        tweetsToReturn += theTweet+" ";	
+			    }
+	        }
+	        //
+	        // tidy done, send it back
+	        //
+	        
+			res.send(tweetsToReturn);
+		}
+		else{
+			
+			// if we didn't get a response from twitter (or an error etc.) 
+			// afterall, we can't be lucky everyday
+			
+			res.send("");
+		}
 		
 		
 	});
